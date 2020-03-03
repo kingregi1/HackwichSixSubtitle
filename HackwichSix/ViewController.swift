ad55e5a817675e26814d4d0ff12a6a960a692161
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-03-03 10:19:39 -1000

    Part 10
    p10

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate{
+    
+    var myfriendsarray=["Marcus","Davion","Ocean"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myfriendsarray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myfriendsarray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
 
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
